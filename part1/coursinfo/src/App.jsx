579dca044c41b71ef72237f3f3ca5b7a6545cc1b
Teo <teosany@icloud.com>
2024-11-14 23:03:46 +0100

    feat: refactor JSX to use separate Part component for each part

--- part1/coursinfo/src/App.jsx
+++ part1/coursinfo/src/App.jsx
@@ -6,15 +6,9 @@ const Header = (props) => {
 const Content = (props) => {
     return (
         <>
-            <p>
-                {props.part1} {props.exercises1}
-            </p>
-            <p>
-                {props.part2} {props.exercises2}
-            </p>
-            <p>
-                {props.part3} {props.exercises3}
-            </p>
+            <Part part={props.part1} ex={props.ex1} />
+            <Part part={props.part2} ex={props.ex2} />
+            <Part part={props.part3} ex={props.ex3} />
         </>
     )
 }
@@ -23,22 +17,27 @@ const Total = (props) => {
         <p>Number of exercises {props.ex1 + props.ex2 + props.ex3}</p>
     )
 }
+const Part = (props) => {
+    return (
+        <p>{props.part} {props.ex}</p>
+    )
+}
 
 
 const App = () => {
     const course = 'Half Stack application development'
     const part1 = 'Fundamentals of React'
-    const exercises1 = 10
+    const ex1 = 10
     const part2 = 'Using props to pass data'
-    const exercises2 = 7
+    const ex2 = 7
     const part3 = 'State of a component'
-    const exercises3 = 14
+    const ex3 = 14
 
     return (
         <div>
             <Header course={course}/>
-            <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
-            <Total ex1={exercises1} ex2={exercises2} ex3={exercises3}/>
+            <Content part1={part1} part2={part2} part3={part3} ex1={ex1} ex2={ex2} ex3={ex3}/>
+            <Total ex1={ex1} ex2={ex2} ex3={ex3}/>
         </div>
     )
 }
