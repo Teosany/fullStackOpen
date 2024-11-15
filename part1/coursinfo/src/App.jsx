b03c7aea9e111052622221a2763b52b2992b3203
Teo <teosany@icloud.com>
2024-11-15 01:57:09 +0100

    feat: refactor App component to use object for course info

--- part1/coursinfo/src/App.jsx
+++ part1/coursinfo/src/App.jsx
@@ -25,27 +25,29 @@ const Part = (props) => {
 
 
 const App = () => {
-    const course = 'Half Stack application development'
-    const parts = [
-        {
-            name: 'Fundamentals of React',
-            exs: 10
-        },
-        {
-            name: 'Using props to pass data',
-            exs: 7
-        },
-        {
-            name: 'State of a component',
-            exs: 14
-        }
-    ]
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exs: 10
+            },
+            {
+                name: 'Using props to pass data',
+                exs: 7
+            },
+            {
+                name: 'State of a component',
+                exs: 14
+            }
+        ]
+    }
 
     return (
         <div>
-            <Header course={course} />
-            <Content parts={parts} />
-            <Total parts={parts} />
+            <Header course={course.name} />
+            <Content parts={course.parts} />
+            <Total parts={course.parts} />
         </div>
     )
 }
