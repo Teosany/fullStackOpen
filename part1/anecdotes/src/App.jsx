da99d84fb5742fef9a5fe308d0b713b66316380e
Teo <teosany@icloud.com>
2024-11-18 21:46:39 +0100

    feat: refactor App component to include voting functionality and display max-voted anecdote

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,6 +1,8 @@
 import {useState} from 'react'
 
-const Button = ({handleClick}) => <button onClick={handleClick}>next anecdote</button>
+const Button = ({handleClick, text}) => <button onClick={handleClick}>{text}</button>
+
+const Title = ({text}) => <h2>{text}</h2>
 
 function App() {
     const anecdotes = [
@@ -14,23 +16,41 @@ function App() {
     ]
 
     const [selected, setSelected] = useState(0)
+    const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
+    const maxVotes = Math.max(...votes)
+    const maxVotesIndex = votes.indexOf(maxVotes)
+    const selectedVotes = votes[selected]
 
-    const handleClick = () => {
+    const handleNext = () => {
         let value
         do {
             value = Math.floor(Math.random() * anecdotes.length)
         } while (value === selected)
         setSelected(value)
     }
+    const handleVote = () => {
+        const newVotes = [...votes]
+        newVotes[selected] += 1
+        setVotes(newVotes)
+    }
 
     return (
         <div>
+            <Title text={'Anecdote of the day'}/>
             <p>
                 {anecdotes[selected]}
             </p>
+            <p>
+                has {selectedVotes} votes
+            </p>
             <div>
-                <Button handleClick={handleClick}></Button>
+                <Button handleClick={handleVote} text={'vote'}></Button>
+                <Button handleClick={handleNext} text={'next anecdote'}></Button>
             </div>
+            <Title text={'Anecdote with most votes'}/>
+            <p>
+                {anecdotes[maxVotesIndex]}
+            </p>
         </div>
     )
 }
